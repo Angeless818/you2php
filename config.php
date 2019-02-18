1f584626af9bb31f400ad5eeba772f96780b68a7
XiaoWei <pgw1314@gmail.com>
2019-02-19 01:53:21 +0800

    初始化

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyBpSx17e7HdvftfghzigobbSxLquQTIw_4');
+define('GJ_CODE', $gl);
+define('SITE_NAME', '我的视频');
+define('TITLENAME', '我的视频');
+define('EN2DEKEY', 'fjdksla;fklawjfowengasihgfeiowajfilajf');
+define('EMAIL', 'pgw1314@gmail.com');
+?>
